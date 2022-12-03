4b97cd9a590c20599ea2b69d618cde0dd831a1fe
Cgotts <cobi.gottschalk@gmail.com>
2022-12-03 12:54:09 -0800

    Added all files, wrote some starter code, and got the basic layout of how the html will look

---
+++ index.js
@@ -0,0 +1,11 @@
+//bring in inquirer
+
+
+
+//employee = [{Manager}, {Engineer}, {Intern} }
+
+//create a function that run inquierer prompt question .then(()=> inquirer.prompt.then)   Main point here: chain the promises so code doesnt end until you dont want anymore teammembers
+
+
+
+//end point = loo through employee array and then convert to html
